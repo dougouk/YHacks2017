5ae17a5608fe4b08aab5cc8b97714dfce2fd4d41
Dongoug <dougouk@gmail.com>
2017-12-02 16:22:24 -0500

    dynamic picutres

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 /**
  * Copyright 2017-present, Facebook, Inc. All rights reserved.
  *
@@ -25,11 +24,10 @@
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 // Imports dependencies and set up http server
-const
-  request = require('request'),
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
+const request = require('request'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express().use(bodyParser.json()); // creates express http server
 
 const picture_1 = 'https://static.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg';
 const picture_2 = 'https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg';
@@ -42,186 +40,197 @@ app.use(bodyParser.json());
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
 
 // Accepts POST requests at /webhook endpoint
 app.post('/webhook', (req, res) => {
     console.log('posting webhook');
 
-  // Parse the request body from the POST
-  let body = req.body;
+    // Parse the request body from the POST
+    let body = req.body;
 
-  // Check the webhook event is from a Page subscription
-  console.log(body.object);
-  if (body.object === 'page') {
+    // Check the webhook event is from a Page subscription
+    console.log(body.object);
+    if (body.object === 'page') {
 
-    // Iterate over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
+        // Iterate over each entry - there may be multiple if batched
+        body.entry.forEach(function(entry) {
 
+            // Get the webhook event. entry.messaging is an array, but
+            // will only ever contain one event, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
 
-      // Get the webhook event. entry.messaging is an array, but
-      // will only ever contain one event, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender ID: ' + sender_psid);
 
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
 
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id;
-      console.log('Sender ID: ' + sender_psid);
+                handlePostback(sender_psid, webhook_event.postback);
+            }
 
-      // Check if the event is a message or postback and
-      // pass the event to the appropriate handler function
-      if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event.message);
-      } else if (webhook_event.postback) {
+        });
+        // Return a '200 OK' response to all events
+        res.status(200).send('EVENT_RECEIVED');
 
-        handlePostback(sender_psid, webhook_event.postback);
-      }
-
-    });
-    // Return a '200 OK' response to all events
-    res.status(200).send('EVENT_RECEIVED');
-
-  } else {
-    // Return a '404 Not Found' if event is not from a page subscription
-    res.status(404).send('NOT FOUND');
-  }
+    } else {
+        // Return a '404 Not Found' if event is not from a page subscription
+        res.status(404).send('NOT FOUND');
+    }
 
 });
 //
-// // server index page
+//  server index page
 // app.get('/', function(req, res) {
 //     res.send('deployed');
 // })
 // Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
 
-  /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = 'trendy_test';
+    /** UPDATE YOUR VERIFY TOKEN **/
+    const VERIFY_TOKEN = 'trendy_test';
 
-  // Parse params from the webhook verification request
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
+    // Parse params from the webhook verification request
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
 
-  // Check if a token and mode were sent
-  if (mode && token) {
+    // Check if a token and mode were sent
+    if (mode && token) {
 
-    // Check the mode and token sent are correct
-    console.log('Request token is ' + token);
-    console.log('Request mode is ' + mode);
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        // Check the mode and token sent are correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
 
-      // Respond with 200 OK and challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
+            // Respond with 200 OK and challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
 
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
     }
-  }
 });
 
 function handleMessage(sender_psid, received_message) {
     let response;
     const message = received_message.text;
 
-      // Checks if the message contains text
+    // Checks if the message contains text
     if (message) {
-            // Create the payload for a basic text message, which
-            // will be added to the body of our request to the Send API
-            if (message.includes('top 10 trends')) {
-                const title = 'You want the top 10 trends!!';
-                const message = 'Click on a different factors for a different analysis!';
-                response = getTop10Trending(picture_1, title, message);
-            } else {
-                response = {
-                    "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-                }
-            }
-        } else if (received_message.attachments) {
-            // Get the URL of the message attachment
-            let attachment_url = received_message.attachments[0].payload.url;
+        // Create the payload for a basic text message, which
+        // will be added to the body of our request to the Send API
+        if (message.includes('top 10 trends')) {
+            const title = 'You want the top 10 trends!!';
+            const message = 'Click on a different factors for a different analysis!';
+            response = getTop10Trending(picture_1, title, message);
+        } else {
             response = {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "generic",
-                        "elements": [
-                            {
-                                "title": "Is this the right picture?",
-                                "subtitle": "Tap a button to answer.",
-                                "image_url": attachment_url,
-                                "buttons": [
-                                    {
-                                        "type": "postback",
-                                        "title": "Yes!",
-                                        "payload": "yes"
-                                    }, {
-                                        "type": "postback",
-                                        "title": "No!",
-                                        "payload": "no"
-                                    }
-                                ]
-                            }
-                        ]
-                    }
+                "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+            }
+        }
+    } else if (received_message.attachments) {
+        // Get the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [
+                        {
+                            "title": "Is this the right picture?",
+                            "subtitle": "Tap a button to answer.",
+                            "image_url": attachment_url,
+                            "buttons": [
+                                {
+                                    "type": "postback",
+                                    "title": "Yes!",
+                                    "payload": "yes"
+                                }, {
+                                    "type": "postback",
+                                    "title": "No!",
+                                    "payload": "no"
+                                }
+                            ]
+                        }
+                    ]
                 }
             }
         }
+    }
 
-  // Send the response message
-  callSendAPI(sender_psid, response);
+    // Send the response message
+    callSendAPI(sender_psid, response);
 }
 
 function handlePostback(sender_psid, received_postback) {
-  console.log('ok')
-   let response;
-  // Get the payload for the postback
-  let payload = received_postback.payload;
+    console.log('ok')
+    let response;
+    // Get the payload for the postback
+    let payload = received_postback.payload;
 
-  // Set the response based on the postback payload
-  if (payload === 'yes') {
-    response = { "text": "Thanks!" }
-  } else if (payload === 'no') {
-    response = { "text": "Oops, try sending another image." }
-  }
-  // Send the message to acknowledge the postback
-  callSendAPI(sender_psid, response);
+    const title = 'You want the top 10 trends!!';
+    const message = 'Click on a different factors for a different analysis!';
+
+    // Set the response based on the postback payload
+    switch(payload) {
+        case TIME_TO_100K:
+            response = getTop10Trending(picture_2, title, message);
+            break;
+        case TOTAL_FUNDED:
+            response = getTop10Trending(picture_1, title, message);
+            break;
+        case NUM_OF_PLEDGES:
+            response = getTop10Trending(picture_3, title, message);
+            break;
+        case 'yes':
+            response = { 'text': 'Thanks!' }
+            break;
+        case 'no':
+            response = { 'text': 'Opps.' }
+            break;
+    }
+
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
 }
 
 function callSendAPI(sender_psid, response) {
-  // Construct the message body
-  let request_body = {
-    "recipient": {
-      "id": sender_psid
-    },
-    "message": response
-  }
-
-  // Send the HTTP request to the Messenger Platform
-  request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
     }
-  });
-}
 
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": {
+            "access_token": PAGE_ACCESS_TOKEN
+        },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
 
 /*
     Our Functions!!
 */
-
-
 
 function getImageResponse(imageUrl, title, message) {
     return {
@@ -252,6 +261,10 @@ function getImageResponse(imageUrl, title, message) {
     }
 }
 
+const TOTAL_FUNDED = 'total_funded';
+const NUM_OF_PLEDGES = 'num_of_pledges';
+const TIME_TO_100K = 'time_to_100k';
+
 function getTop10Trending(graphImage, title, message) {
     return {
         "attachment": {
@@ -267,15 +280,15 @@ function getTop10Trending(graphImage, title, message) {
                             {
                                 "type": "postback",
                                 "title": "Total funded",
-                                "payload": "total_funded"
+                                "payload": TOTAL_FUNDED
                             }, {
                                 "type": "postback",
                                 "title": "Number of pledges",
-                                "payload": "num_of_pledge"
+                                "payload": NUM_OF_PLEDGES
                             }, {
                                 "type": "postback",
                                 "title": "Time taken to raise more than $100,000",
-                                "payload": "time_to_100k"
+                                "payload": TIME_TO_100K
                             }
                         ]
                     }
